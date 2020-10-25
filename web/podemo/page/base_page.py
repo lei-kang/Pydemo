0aeddd9d374a0772134a1c8bbae7aa1cd4a214d6
kang'lei <15028509068@163.com>
2020-10-25 22:36:32 +0800

    po 作业

--- web/podemo/page/base_page.py
+++ web/podemo/page/base_page.py
@@ -6,7 +6,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.remote.webdriver import WebDriver
-
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
 class BasePage():
     base_url = ""
@@ -23,8 +25,13 @@ class BasePage():
         if self.base_url != "":
             self.driver.get(self.base_url)
 
-    def find(self,by,local):
-        return self.driver.find_element(by,local)
+    def find(self,by,locator):
+        return self.driver.find_element(by,locator)
 
-    def finds(self,by,local):
-        return self.driver.find_elements(by,local)
+    def finds(self,by,locator):
+        return self.driver.find_elements(by,locator)
+
+    def wait(self,locator,timeout=10):
+        element:WebElement = WebDriverWait(self.driver,timeout).until(expected_conditions.element_to_be_clickable(locator))
+        return element
+
--- web/podemo/page/indexpage.py
+++ web/podemo/page/indexpage.py
@@ -6,15 +6,29 @@
 from time import sleep
 
 from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
 
 from web.podemo.page.add_menber_page import AddMenberPage
 from web.podemo.page.base_page import BasePage
-
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
 class IndexPage(BasePage):
     base_url = "https://work.weixin.qq.com/wework_admin/frame#index"
     def goto_menber(self):
         self.find(By.CSS_SELECTOR,"#menu_contacts").click()
-        sleep(2)
-        self.find(By.CSS_SELECTOR, ".js_has_member>div:nth-child(1)>a:nth-child(2)").click()
+        locator = (By.CSS_SELECTOR, ".js_has_member>div:nth-child(1)>a:nth-child(2)")
+        # element:WebElement = WebDriverWait(self.driver,10).until(expected_conditions.element_to_be_clickable(locator))
+        # self.find(By.CSS_SELECTOR, ".js_has_member>div:nth-child(1)>a:nth-child(2)").click()
+        self.wait(locator).click()
+        # 借助WebDriverWait 显示等待判断跳转按钮是否可以点击，同时判断是否跳转成功即茶轴跳转后的一个元素
+        # def wait_for_next(x: WebDriver):
+        #     try:
+        #         x.find_element(*locator).click()
+        #         return x.find_element(By.ID, "username")
+        #     except:
+        #         return False
+        #
+        # WebDriverWait(self.driver, 10).until(wait_for_next)
         return AddMenberPage(self.driver)
